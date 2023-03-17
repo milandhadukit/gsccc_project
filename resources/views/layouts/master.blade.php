9bff82c5d04fe6c15414371ae4e2a37624bd8edd
milandhadukit <milandhaduk.siliconithub@gmail.com>
2023-03-17 18:49:20 +0530

    admin panel design

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="description" content="CodedThemes">
+    <meta name="keywords"
+        content=" Admin , Responsive, Landing, Bootstrap, App, Template, Mobile, iOS, Android, apple, creative app">
+    <meta name="author" content="CodedThemes">
+    <!-- Favicon icon -->
+    {{-- <link rel="icon" href="assets/images/favicon.ico" type="image/x-icon"> --}}
+    <link href="{{ URL::asset('assets/images/favicon.ico') }}" rel="icon" type="image/x-icon" />
+
+
+    <!-- Google font-->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet">
+
+
+
+    <!-- Required Fremwork -->
+    {{-- <link rel="stylesheet" type="text/css" href="assets/css/bootstrap/css/bootstrap.min.css"> --}}
+    <link href="{{ URL::asset('assets/css/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+
+    <!-- themify-icons line icon -->
+    {{-- <link rel="stylesheet" type="text/css" href="assets/icon/themify-icons/themify-icons.css"> --}}
+    <link href="{{ URL::asset('assets/icon/themify-icons/themify-icons.css') }}" rel="stylesheet" type="text/css" />
+
+    <!-- ico font -->
+    {{-- <link rel="stylesheet" type="text/css" href="assets/icon/icofont/css/icofont.css"> --}}
+    <link href="{{ URL::asset('assets/icon/icofont/css/icofont.css') }}" rel="stylesheet" type="text/css" />
+    <!-- Style.css -->
+    {{-- <link rel="stylesheet" type="text/css" href="assets/css/style.css"> --}}
+    <link href="{{ URL::asset('assets/css/style.css') }}" rel="stylesheet" type="text/css" />
+    {{-- <link rel="stylesheet" type="text/css" href="assets/css/jquery.mCustomScrollbar.css"> --}}
+    <link href="{{ URL::asset('assets/css/jquery.mCustomScrollbar.css') }}" rel="stylesheet" type="text/css" />
+
+    {{-- <link href="{{ URL::asset('assets/css/style.css'); }}" rel="stylesheet" />
+      <script src="{{ asset('assets/plugins/jquery-1.10.2.js')}}"></script> --}}
+    <title>gsccc</title>
+</head>
+
+<body>
+    {{-- <body> --}}
+
+
+    <div id="pcoded" class="pcoded">
+        <div class="pcoded-overlay-box"></div>
+        <div class="pcoded-container navbar-wrapper">
+
+            @include('body.sidebar_nav')
+            @yield('content')
+            {{-- @include('pages.Deshboard') --}}
+
+
+
+
+
+            <!-- Pre-loader start -->
+            <div class="theme-loader">
+                <div class="ball-scale">
+                    <div class='contain'>
+                        <div class="ring">
+                            <div class="frame"></div>
+                        </div>
+                        <div class="ring">
+                            <div class="frame"></div>
+                        </div>
+                        <div class="ring">
+
+                            <div class="frame"></div>
+                        </div>
+                        <div class="ring">
+                            <div class="frame"></div>
+                        </div>
+                        <div class="ring">
+                            <div class="frame"></div>
+                        </div>
+                        <div class="ring">
+                            <div class="frame"></div>
+                        </div>
+                        <div class="ring">
+                            <div class="frame"></div>
+                        </div>
+                        <div class="ring">
+                            <div class="frame"></div>
+                        </div>
+                        <div class="ring">
+                            <div class="frame"></div>
+                        </div>
+                        <div class="ring">
+                            <div class="frame"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Pre-loader end -->
+
+        </div>
+    </div>
+    <div class="fixed-button">
+        <a href="https://codedthemes.com/item/guru-able-admin-template/" target="_blank" class="btn btn-md btn-primary">
+            <i class="fa fa-shopping-cart" aria-hidden="true"></i> Upgrade To Pro
+        </a>
+    </div>
+    </div>
+    </div>
+
+
+
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- Required Jquery -->
+<script type="text/javascript" src="assets/js/jquery/jquery.min.js"></script>
+<script type="text/javascript" src="assets/js/jquery-ui/jquery-ui.min.js"></script>
+<script type="text/javascript" src="assets/js/popper.js/popper.min.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap/js/bootstrap.min.js"></script>
+<!-- jquery slimscroll js -->
+<script type="text/javascript" src="assets/js/jquery-slimscroll/jquery.slimscroll.js"></script>
+<!-- modernizr js -->
+<script type="text/javascript" src="assets/js/modernizr/modernizr.js"></script>
+<!-- am chart -->
+<script src="assets/pages/widget/amchart/amcharts.min.js"></script>
+<script src="assets/pages/widget/amchart/serial.min.js"></script>
+<!-- Todo js -->
+<script type="text/javascript " src="assets/pages/todo/todo.js "></script>
+<!-- Custom js -->
+<script type="text/javascript" src="assets/pages/dashboard/custom-dashboard.js"></script>
+<script type="text/javascript" src="assets/js/script.js"></script>
+<script type="text/javascript " src="assets/js/SmoothScroll.js"></script>
+
+<script src="assets/js/pcoded.min.js"></script>
+<script src="assets/js/demo-12.js"></script>
+<script src="assets/js/jquery.mCustomScrollbar.concat.min.js"></script>
+
+<script>
+    var $window = $(window);
+    var nav = $('.fixed-button');
+    $window.scroll(function() {
+        if ($window.scrollTop() >= 200) {
+            nav.addClass('active');
+        } else {
+            nav.removeClass('active');
+        }
+    });
+</script>
+
+
+</body>
+
+</html>
